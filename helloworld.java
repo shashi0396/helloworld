80008d5808c209066aa15fffdb8e31625c2cc8bd
shashi0396 <ubuntu@ip-172-31-11-42.us-east-2.compute.internal>
2023-01-11 06:19:45 +0000

    added helloworld program

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+package com.tcc.java.programs;
+ 
+/**
+ * Java Program to Print "Hello World" 10 times using for loop
+ */
+public class HelloWorldLoop {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 10; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
